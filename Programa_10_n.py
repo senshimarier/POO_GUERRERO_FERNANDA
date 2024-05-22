679bd643e120b352fdb5fff1895708e5d4fbacff
Maria Fernanda Guerrero Calderón <169078581+senshimarier@users.noreply.github.com>
2024-05-22 10:58:09 -0600

    Programa 10 - Sumas

---
+++ Programa_10_n.py
@@ -0,0 +1,16 @@
+
+while True:
+  print ("Inserta dos números:")
+
+  num1 = int(input())
+  num2 = int(input())
+
+  suma = num1 + num2
+  print (suma)
+
+  mensaje = input("Si desea detener oprima 'n'")
+
+  if mensaje == "n":
+    print ("Programa finalizado")
+    break
+  
