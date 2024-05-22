6e3a1efa3a97f5d59cfbd272716df2b2bc2b2aa7
Maria Fernanda Guerrero Calderón <169078581+senshimarier@users.noreply.github.com>
2024-05-21 20:28:35 -0600

    Programa 9 - Contraseña

---
+++ Programa_9_login.py
@@ -0,0 +1,12 @@
+print ("Ingrese la contraseña")
+contraseña = str(input())
+
+while contraseña != "POO123":
+    print ("Contraseña incorrecta")
+  
+    print (" ")
+  
+    print ("Ingrese la contraseña")
+    contraseña = str(input())
+    
+    break
