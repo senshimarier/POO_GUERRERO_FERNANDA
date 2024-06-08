78cda8792ea7ccfdfd5fef827f136403105b5a16
Maria Fernanda Guerrero Calderón <169078581+senshimarier@users.noreply.github.com>
2024-06-08 15:37:39 -0600

    Programa 21 – Tupla desempaquetada

---
+++ Programa_21_tupla.py
@@ -0,0 +1,13 @@
+#Crea un programa que contenga una tupla con información de una persona y desempaqueta la información en 5 diferentes variables para imprimirlas por separado (por ejemplo nombre, edad…).
+#Commit: Programa 21 – Tupla desempaquetada
+
+tupla = ("Juan", "Perez", "25" , "México", "Casado")
+print (tupla)
+print (" ")
+nombre, apellido, edad, pais, estado = tupla
+
+print(nombre)
+print(apellido)
+print (edad)
+print (pais)
+print (estado)
